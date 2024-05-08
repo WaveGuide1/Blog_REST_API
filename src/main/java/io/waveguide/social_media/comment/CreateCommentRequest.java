baa61cd5949147845d57805f90a180ff6be3be7b
WaveGuide1 <barthalaekwe@gmail.com>
2024-05-08 03:37:11 +0100

    Pagination

--- src/main/java/io/waveguide/social_media/comment/CreateCommentRequest.java
+++ src/main/java/io/waveguide/social_media/comment/CreateCommentRequest.java
@@ -2,6 +2,7 @@ package io.waveguide.social_media.comment;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CreateCommentRequest {
 
     @NotBlank(message = "Comment message is required")
